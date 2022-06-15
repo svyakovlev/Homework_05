4d541914644f9e5ac6c8c6183e38b3c6f8743d2f
Сергей <s.v.yakovlev@yandex.ru>
2022-06-15 21:35:14 +0400

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         //Задача №1
+        int[] array1 = new int[3];
+        array1[0] = 1;
+        array1[1] = 2;
+        array1[2] = 3;
 
+        double[] array2 = {1.57, 7.654, 9.986};
+
+        boolean[] array3 = {false, true, true};
     }
 }
